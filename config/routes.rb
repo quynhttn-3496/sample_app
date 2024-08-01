96dc7164ee1421e0ef853602f4b9ae125971d88a
quynhttn-3496 <tran.thi.nhu.quynh@sun-asterisk.com>
2024-08-01 17:01:14 +0700

    chapter_8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
     get "static_pages/home"
     get "static_pages/help"
-    
+
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
     resources :users, only: %i(new create show)
   end
 end
