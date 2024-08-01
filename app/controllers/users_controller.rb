0806f2cc7ad71b83fe52812dad786453e74e51ec
quynhttn-3496 <tran.thi.nhu.quynh@sun-asterisk.com>
2024-08-01 16:07:45 +0700

    chapter_7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "not_found_user"
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t "created_successful"
+      redirect_to @user, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::ATTRIBUTE_PERMITTED
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     get "static_pages/home"
     get "static_pages/help"
+    
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new create show)
   end
 end
