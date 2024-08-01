ac336efd442ecaddd69667cd8730a094172ccfd6
quynhttn-3496 <tran.thi.nhu.quynh@sun-asterisk.com>
2024-08-01 09:26:40 +0700

    chapter_6

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :name, presence: true, length: {maximum: Settings.MAX_LENGTH_NAME}
+
+  validates :email, presence: true, length:
+    {maximum: Settings.MAX_LENGTH_EMAIL},
+    format: {with: Settings.VALID_EMAIL_REGEX}, uniqueness: true
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
