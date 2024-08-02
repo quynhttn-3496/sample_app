81d83f160f657687217ca648d2931cd71789f0e3
quynhttn-3496 <tran.thi.nhu.quynh@sun-asterisk.com>
2024-08-02 16:45:30 +0700

    chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params.dig(:session, :email)&.downcase
-    if user&.authenticate params.dig(:session, :password)
+    if user.try(:authenticate, params.dig(:session, :password))
       log_in user
+      params.dig(:session, :remember_me) == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = t "invalid_email_password_combination"
